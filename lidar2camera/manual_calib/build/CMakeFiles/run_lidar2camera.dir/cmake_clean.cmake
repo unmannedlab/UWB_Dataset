57da0f0c27a831d807127cd0b49c75c34ad2e52e
Jia Huang <62807480+jh0329@users.noreply.github.com>
2024-01-14 20:39:39 -0600

    Add files via upload

---
+++ lidar2camera/manual_calib/build/CMakeFiles/run_lidar2camera.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/run_lidar2camera.dir/src/run_lidar2camera.cpp.o"
+  "../bin/run_lidar2camera.pdb"
+  "../bin/run_lidar2camera"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/run_lidar2camera.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
